7b3fffdd4774bbe3972b6d17dc5633a7213f7f68
Jordan Bourbonnais <clonage@gmail.com>
2023-01-14 08:22:30 +0000

    gffgfg

--- files/index.986.js
+++ files/index.986.js
@@ -1,10 +1,10 @@
 (function(){const r=document.createElement("link").relList;
 if(r&&r.supports&&r.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))i(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const c of t.addedNodes)c.tagName==="LINK"&&c.rel==="modulepreload"&&i(c)}).observe(document,{childList:!0,subtree:!0});function o(e){const t={};
 return e.integrity&&(t.integrity=e.integrity),e.referrerpolicy&&(t.referrerPolicy=e.referrerpolicy),e.crossorigin==="use-credentials"?t.credentials="include":e.crossorigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function i(e){if(e.ep)return;e.ep=!0;const t=o(e);fetch(e.href,t)}})();
-function s(n){let r=0;const o=i=>{r=i,n.innerHTML=`Voir nos forfaits`};n.addEventListener("click",()=>o(r+1)),o(0)}document.querySelector("#app").innerHTML=`
+function s(n){let r=0;const o=i=>{r=i,n.innerHTML=`Voir nos cellulaires et forfaits`};n.addEventListener("click",()=>o(r+1)),o(0)}document.querySelector("#app").innerHTML=`
   <div>
-    <img src="https://voyance.cc/files/images/vite.svg" class="logo" alt="Voyance Francophone" />
-    <h2>Voyance Francophone en-ligne!</h2>
+    <img src="https://mixcells.com/files/images/vite.svg" class="logo" alt="MixCells" />
+    <h2>MixCells!</h2>
     <p class="read-the-docs">
     </p>
     <div class="card">
