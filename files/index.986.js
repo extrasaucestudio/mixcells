eb4d17bba3fa46e797b992953b2caf87d9d8e478
Jordan Bourbonnais <clonage@gmail.com>
2023-01-14 08:35:13 +0000

    jjhjh

--- files/index.986.js
+++ files/index.986.js
@@ -3,7 +3,7 @@ if(r&&r.supports&&r.supports("modulepreload"))return;for(const e of document.que
 return e.integrity&&(t.integrity=e.integrity),e.referrerpolicy&&(t.referrerPolicy=e.referrerpolicy),e.crossorigin==="use-credentials"?t.credentials="include":e.crossorigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function i(e){if(e.ep)return;e.ep=!0;const t=o(e);fetch(e.href,t)}})();
 function s(n){let r=0;const o=i=>{r=i,n.innerHTML=`Voir nos cellulaires et forfaits`};n.addEventListener("click",()=>o(r+1)),o(0)}document.querySelector("#app").innerHTML=`
   <div>
-    <img src="https://mixcells.com/files/images/vite.svg" class="logo" alt="MixCells" />
+    <img src="files/images/vite.svg" class="logo" alt="MixCells" />
     <h2>MixCells!</h2>
     <p class="read-the-docs">
     </p>
